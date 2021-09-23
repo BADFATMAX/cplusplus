fc1f9c3cfea1832a28e50ebf4d5bca49c0ab7007
BADFATMAX <44487962+BADFATMAX@users.noreply.github.com>
2021-09-23 22:49:04 +0700

    Add files via upload

---
+++ task0a/task0a/cmake-build-debug/CMakeFiles/task0a.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task0a.dir/main.cpp.obj"
+  "CMakeFiles/task0a.dir/module1.cpp.obj"
+  "CMakeFiles/task0a.dir/module2.cpp.obj"
+  "libtask0a.dll.a"
+  "task0a.exe"
+  "task0a.exe.manifest"
+  "task0a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task0a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
